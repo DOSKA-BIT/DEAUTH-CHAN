b0aa4dc94405cd0026a9bda77220383d9ebd407c
DOSKA-BIT <proyectosapps50@gmail.com>
2026-08-15 16:36:26 -0300

    Add Mascota class header file

---
+++ src/Mascota.h
@@ -0,0 +1,45 @@
+#ifndef MASCOTA_H
+#define MASCOTA_H
+
+#include <TFT_eSPI.h>
+#include "config.h"
+
+class Mascota {
+public:
+    Mascota();
+    void init(TFT_eSPI *tft);
+    void update();
+    void setEstado(EstadoMascota estado);
+    void dibujar();
+    
+    void incrementarHandshakes();
+    void setRedesEncontradas(int cantidad);
+    
+    int getHandshakes() { return handshakes; }
+    int getRedesEncontradas() { return redesEncontradas; }
+    
+private:
+    TFT_eSPI *display;
+    EstadoMascota estadoActual;
+    EstadoMascota estadoAnterior;
+    
+    unsigned long lastFrame;
+    unsigned long estadoStartTime;
+    uint8_t frameAnimacion;
+    
+    int handshakes;
+    int redesEncontradas;
+    
+    void dibujarSprite();
+    void dibujarUI();
+    void dibujarIdle();
+    void dibujarScanning();
+    void dibujarHappy();
+    void dibujarAttack();
+    void dibujarSleep();
+    
+    // Sprites simplificados (32x32)
+    void dibujarCaraBase(uint16_t color);
+};
+
+#endif
