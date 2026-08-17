633b16489c84b3f3557b2669836deeda376daea1
DOSKA-BIT <proyectosapps50@gmail.com>
2026-08-16 21:00:27 -0300

    Update WiFiHunter.h

--- src/WiFiHunter.h
+++ src/WiFiHunter.h
@@ -4,14 +4,18 @@
 #include <WiFi.h>
 #include "config.h"
 
+// Typedef para el callback
+typedef void (*HandshakeCallback)(const uint8_t* frame, uint32_t len);
+
 class WiFiHunter {
 public:
     void begin();
     void scan(RedInfo* resultados, int maxRedes, int& encontradas);
     void deauth(const RedInfo& red);
+    void setHandshakeCallback(HandshakeCallback cb);
     
 private:
-    bool promiscuoActivado = false;
+    static void promiscuousCallback(void* buf, wifi_promiscuous_pkt_type_t type);
 };
 
 #endif
