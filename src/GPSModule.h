ea6e6174b4ef74632f93a8639d1f394ae927161d
DOSKA-BIT <proyectosapps50@gmail.com>
2026-08-16 21:10:47 -0300

    Create GPSModule.h

---
+++ src/GPSModule.h
@@ -0,0 +1,29 @@
+#ifndef GPSMODULE_H
+#define GPSMODULE_H
+
+#include <TinyGPS++.h>
+#include <HardwareSerial.h>
+
+struct GPSData {
+    double lat;
+    double lng;
+    double altitude;
+    int satellites;
+    bool valid;
+    char timestamp[32];
+};
+
+class GPSModule {
+public:
+    void begin();
+    void update();
+    GPSData getData();
+    bool hasFix();
+    
+private:
+    TinyGPSPlus gps;
+    HardwareSerial* gpsSerial;
+    unsigned long lastValidFix = 0;
+};
+
+#endif
