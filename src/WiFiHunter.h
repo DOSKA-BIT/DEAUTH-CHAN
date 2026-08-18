c39f5f6217a94fff6950fbc86fbb7318c0df0f04
DOSKA-BIT <proyectosapps50@gmail.com>
2026-08-18 08:46:18 -0300

    Update WiFiHunter.h

--- src/WiFiHunter.h
+++ src/WiFiHunter.h
@@ -5,17 +5,20 @@
 #include "config.h"
 
 typedef void (*HandshakeCallback)(const uint8_t* frame, uint32_t len);
+
 #define MAX_PENDING_HS 5
+#define MAX_CLIENTES 20
 
 struct PendingHS {
     uint8_t frame[256];
     uint32_t len;
 };
 
-// Estructura para almacenar información de un cliente
 struct ClienteInfo {
     uint8_t mac[6];
     int rssi;
+    int canal;
+    uint8_t bssid[6];
 };
 
 class WiFiHunter {
@@ -25,10 +28,7 @@ public:
     bool isScanDone();
     void getScanResults(RedInfo* resultados, int maxRedes, int& encontradas);
     
-    // NUEVO: escanea clientes de una red específica
-    void scanClients(const uint8_t* bssid, ClienteInfo* clientes, int maxClientes, int& encontrados);
-    
-    // NUEVO: deauth mejorado con ráfaga y canal automático
+    void scanClients(const uint8_t* bssid, int canal, ClienteInfo* clientes, int maxClientes, int& encontrados);
     void deauth(const RedInfo& red, const uint8_t* clienteMac = nullptr, int numPaquetes = 20);
     
     void setHandshakeCallback(HandshakeCallback cb);
@@ -36,6 +36,7 @@ public:
     
 private:
     static void promiscuousCallback(void* buf, wifi_promiscuous_pkt_type_t type);
+    void sendDeauthFrame(const uint8_t* bssid, const uint8_t* clienteMac, int channel);
     
     PendingHS pending[MAX_PENDING_HS];
     volatile int head = 0;
@@ -43,9 +44,6 @@ private:
     
     static HandshakeCallback handshakeCB;
     static WiFiHunter* instance;
-    
-    // NUEVO: método interno para enviar un frame de deauth
-    void sendDeauthFrame(const uint8_t* bssid, const uint8_t* clienteMac, int channel);
 };
 
 #endif
