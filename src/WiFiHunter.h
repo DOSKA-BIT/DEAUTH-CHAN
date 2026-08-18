fed5da2a18373a1a125fb3ece80ca72e39c532a5
DOSKA-BIT <proyectosapps50@gmail.com>
2026-08-18 10:14:33 -0300

    Refactor packet structures and client scan methods

--- src/WiFiHunter.h
+++ src/WiFiHunter.h
@@ -8,7 +8,7 @@ typedef void (*HandshakeCallback)(const uint8_t* frame, uint32_t len);
 
 #define MAX_PENDING_HS 5
 #define MAX_CLIENTES 20
-#define BUFFER_PAQUETES 100
+#define MAX_PAQUETES_BUFFER 100
 
 struct PendingHS {
     uint8_t frame[256];
@@ -23,10 +23,9 @@ struct ClienteInfo {
     unsigned long lastSeen;
 };
 
-struct PaqueteCapturado {
-    uint8_t macOrigen[6];
-    uint8_t macDestino[6];
-    uint8_t bssid[6];
+struct PaqueteRaw {
+    uint8_t data[128];
+    uint16_t len;
     int rssi;
     int canal;
     unsigned long timestamp;
@@ -39,24 +38,33 @@ public:
     bool isScanDone();
     void getScanResults(RedInfo* resultados, int maxRedes, int& encontradas);
     
-    void scanClients(const uint8_t* bssid, int canal, ClienteInfo* clientes, int maxClientes, int& encontrados);
+    void startClientScan(const uint8_t* bssid, int canal);
+    void stopClientScan();
+    bool isClientScanActive();
+    void getClients(ClienteInfo* clientes, int maxClientes, int& encontrados);
+    
     void deauth(const RedInfo& red, const uint8_t* clienteMac = nullptr, int numPaquetes = 20);
+    void deauthAll(const RedInfo& red, int numPaquetes = 10);
     
     void setHandshakeCallback(HandshakeCallback cb);
     void processPendingHandshakes();
-    void processPackets();  // Procesa paquetes capturados para extraer clientes
     
 private:
     static void promiscuousCallback(void* buf, wifi_promiscuous_pkt_type_t type);
     void sendDeauthFrame(const uint8_t* bssid, const uint8_t* clienteMac, int channel);
+    void procesarPaquete(const uint8_t* frame, uint16_t len, int rssi, int canal);
+    bool esClienteValido(const uint8_t* mac, const uint8_t* bssid);
+    void actualizarCliente(const uint8_t* mac, const uint8_t* bssid, int rssi, int canal);
     
     PendingHS pending[MAX_PENDING_HS];
     volatile int head = 0;
     volatile int tail = 0;
     
-    PaqueteCapturado packetBuffer[BUFFER_PAQUETES];
-    volatile int packetHead = 0;
-    volatile int packetTail = 0;
+    ClienteInfo clientes[MAX_CLIENTES];
+    int numClientes = 0;
+    uint8_t bssidObjetivo[6];
+    bool escaneandoClientes = false;
+    unsigned long lastClientUpdate = 0;
     
     static HandshakeCallback handshakeCB;
     static WiFiHunter* instance;
