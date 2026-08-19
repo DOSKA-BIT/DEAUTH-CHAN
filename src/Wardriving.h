20cfc44a3670fdcc1cd75e5ca3351f737ef0b675
DOSKA-BIT <proyectosapps50@gmail.com>
2026-08-18 23:02:29 -0300

    Create Wardriving.h

---
+++ src/Wardriving.h
@@ -0,0 +1,23 @@
+#ifndef WARDRIVING_H
+#define WARDRIVING_H
+
+#include <SD.h>
+#include <FS.h>
+#include "config.h"
+#include "GPSModule.h"
+
+class Wardriving {
+public:
+    void begin();
+    void saveNetwork(const RedInfo& red, const GPSData& pos);
+    void exportKML();
+    void setFilterByEncryption(bool enabled, int authMode);
+    
+private:
+    File csvFile;
+    bool filterEnabled = false;
+    int filterAuthMode = WIFI_AUTH_OPEN;
+    void ensureHeader();
+};
+
+#endif
